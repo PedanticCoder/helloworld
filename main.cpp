dfe682653117e6802a5221460bdd93c232c5596b
Artem <kurov.artem308@gmail.com>
2019-10-12 16:26:10 +0300

    delete tests

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-    std::cout << "Build "<< PROJECT_VERSION_PATCH << std::endl;
-    std::cout << "Hello World" << std::endl;
+    std::cout << "build "<< PROJECT_VERSION_PATCH << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }
